f8c6367a38cec52f47fb57ae0993871565ab468b
Yeskendir <eskender.kalimbetov@gmail.com>
2025-12-12 17:15:16 +0500

    Add files via upload

---
+++ py/advanced_programming-/1.py
@@ -0,0 +1,8 @@
+fn = str(input("Your first name?"))
+ln = str(input("Your last name?"))
+age = int(input("How old are you?"))
+ph = int(input("Your phone number?"))
+
+print("Your First and Last name", fn , ln )
+print("Your age", age )
+print("Your number", ph)
