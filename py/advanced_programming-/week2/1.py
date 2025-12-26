8956440deecea652fe40c7e963cde4fb9ea27bee
Yeskendir-CS2412 <eskender.kalimbetov@gmail.com>
2025-12-26 16:28:59 +0500

    a

--- py/advanced_programming-/week2/1.py
+++ py/advanced_programming-/week2/1.py
@@ -2,7 +2,7 @@ arrow1 = ">>-->"
 arrow2 = "<--<<" 
 legolas = str(input("Enter the string: ")) 
 arrow = 0
-for i in range(len(legolas)-4): 
+for i in range(len(legolas-4)): 
     if legolas[i:i+5] == arrow1 or legolas[i:i+5] == arrow2: 
         arrow += 1 
 print("arrows.count:", arrow)
